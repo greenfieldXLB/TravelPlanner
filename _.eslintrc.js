acd36416249478c72a5e6df346f8baa725d63070
tiffanycchoy <tiffany.c.choy@gmail.com>
2017-08-07 16:51:49 -0700

    Add files via upload

---
+++ _.eslintrc.js
@@ -0,0 +1,10 @@
+/**
+ * These rules enforce the Hack Reactor Style Guide
+ *
+ * Visit this repo for more information:
+ *   https://github.com/reactorcore/eslint-config-hackreactor
+ */
+
+module.exports = {
+  extends: './node_modules/eslint-config-hackreactor/index.js'
+};
