19eb5a064467ea90a974f2d1e45e3585ff32697c
tiffanycchoy <tiffany.c.choy@gmail.com>
2017-08-07 19:23:17 -0700

    starter files

--- server/index.js
+++ server/index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var bodyParser = require('body-parser');
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
 // var items = require('../database-mysql');
-// var items = require('../database-mongo');
+var items = require('../database-mongo');
 
 var app = express();
 
 // UNCOMMENT FOR REACT
-// app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.static(__dirname + '/../react-client/dist'));
 
 // UNCOMMENT FOR ANGULAR
 // app.use(express.static(__dirname + '/../angular-client'));
@@ -26,4 +26,3 @@ app.get('/items', function (req, res) {
 app.listen(3000, function() {
   console.log('listening on port 3000!');
 });
-
