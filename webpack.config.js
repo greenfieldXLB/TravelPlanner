5eebcc18339a5ad284d22601b8f377f91f1125a4
Greg Susko <gsusko1@gmail.com>
2017-08-13 13:38:28 -0700

    update for heroku

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,6 @@ module.exports = {
     filename: 'bundle.js',
     path: DIST_DIR
   },
-  resolveLoader: {
-        fallback: '/usr/local/lib/node_modules'
-  },
   module : {
     loaders : [
       {
