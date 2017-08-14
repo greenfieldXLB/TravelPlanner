a6017c31bdccc4210f39a6d99635e4ee6ebef9f2
Greg Susko <gsusko1@gmail.com>
2017-08-13 18:10:38 -0700

    cleanup db index.js

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -1,16 +1,13 @@
 var mongoose = require('mongoose');
-var uristring =
-   process.env.MONGOLAB_URI ||
-   process.env.MONGOHQ_URL ||
-   'mongodb://localhost/TravelPlanner';
+var uristring = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/TravelPlanner';
 
-   mongoose.connect(uristring, function (err, res) {
-     if (err) {
-     console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-     } else {
-     console.log ('Succeeded connected to: ' + uristring);
-     }
-   });
+  mongoose.connect(uristring, function (err, res) {
+    if (err) {
+      console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+    } else {
+      console.log ('Succeeded connected to: ' + uristring);
+    }
+  });
 
 var db = mongoose.connection;
 
