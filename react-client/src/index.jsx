e9d2dc20f888d4982492e4cf5727dfbe7e6e3852
Greg Susko <gsusko1@gmail.com>
2017-08-15 09:05:52 -0700

    update

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -5,7 +5,6 @@ import Hotel from './components/hotel.jsx'
 import Flights from './components/Flights.jsx';
 import config from '../../config.js';
 const FlightAPI = require('qpx-express');
-import SearchBar from './components/SearchBar.jsx';
 
 
 class App extends React.Component {
@@ -35,7 +34,6 @@ class App extends React.Component {
       hotels: [],
       airportCodes: {}
     }
-    this.onSearch = this.onSearch.bind(this);
   }
 
   handleClick() {
@@ -164,8 +162,8 @@ class App extends React.Component {
       fetch(`https://www.air-port-codes.com/api/v1/multi?term=${arrivalLoc}`, {
         headers: {
           Accept: "application/json",
-          "APC-Auth": config.APCAuth,
-          "APC-Auth-Secret": config.APCSecret
+          "APC-Auth": "ea0eb61a9e",
+          "APC-Auth-Secret": "4b35787cfc26306"
         },
         method: "POST"
       })
@@ -209,25 +207,11 @@ class App extends React.Component {
     this.state.savedChoices[0].flights = saved;
   }
 
-  onSearch (departureLocation, arrivalLocation, departureDate, returnDate) {
-    console.log('the departure location is: ', departureLocation);
-    console.log('the arrival location is: ', arrivalLocation);
-    console.log('the departure date is: ', departureDate);
-    console.log('the return date is: ', returnDate);
-    this.setState({
-      departureLocation: departureLocation,
-      arrivalLocation: arrivalLocation,
-      departureDate: departureDate,
-      returnDate: returnDate
-    })
-  }
-
   render () {
     return (
       <div>
         <h1>Trip Planner</h1>
-        <SearchBar onSearch = {this.onSearch}/>
-        <Hotel handleClick={this.handleClick.bind(this)} hotels = {this.state.hotels} />
+        <Hotel  handleClick={this.handleClick.bind(this)} hotels = {this.state.hotels} />
         <div>
           <h2>Flights</h2>
           <Flights handleFlightClick={this.handleFlightClick.bind(this)} flights={this.state.flights}/>
