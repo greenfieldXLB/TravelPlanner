c1a3f22ed9b84799a0623af1eed6e15127f427f6
woshiwaid <woshiwadid@gmail.com>
2017-08-16 11:27:28 -0700

    fix yelpattr

--- server/yelpattraction/yelpattraction.js
+++ server/yelpattraction/yelpattraction.js
@@ -1,5 +1,5 @@
 const yelp = require('yelp-fusion');
-// const yelpConfig = require('../../config.js');
+const yelpConfig = require('../../config.js');
 
 
 var searchAttr = function (searchCity, callback){
