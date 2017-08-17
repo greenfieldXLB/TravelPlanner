8501a35bc1c54dc4358fabdcbed371721c193146
tiffanycchoy <tiffany.c.choy@gmail.com>
2017-08-17 11:37:46 -0700

    rebase

--- react-client/src/components/HotelEntry.jsx
+++ react-client/src/components/HotelEntry.jsx
@@ -11,12 +11,7 @@ class HotelEntry extends React.Component {
 
   handleHotelClick(hotel, event){
     this.props.handleHotelClick(hotel, event);
-<<<<<<< HEAD
-||||||| merged common ancestors
-    
-=======
 
->>>>>>> formatting lodging, attractions, restaurants
   }
 
   render () {
@@ -25,21 +20,13 @@ class HotelEntry extends React.Component {
       <div onClick={(e) => (this.handleHotelClick(this.props.hotel, e))}>
           <div className='avoid-clicks'>
           <div> <br></br></div>
+          <div> {this.props.hotel.price}</div>
           <div className='avoid-clicks'><b>{this.props.hotel.name}</b></div>
           <div className='avoid-clicks'>{this.props.hotel.location.display_address.join(', ')}</div>
-          <div> {this.props.hotel.price}</div>
           <img className='avoid-clicks' src={this.props.hotel.image_url}  width="150" height = "150"></img>
           </div>
       </div>
     )
   }
-<<<<<<< HEAD
 }
 export default HotelEntry;
-||||||| merged common ancestors
-}   
-export default HotelEntry;
-=======
-}
-export default HotelEntry;
->>>>>>> formatting lodging, attractions, restaurants
