8d0d4476a27e10ad1f027d4863a7bfe6e04db5f9
novyQ <novaqiu@gmail.com>
2017-08-16 17:14:15 -0700

    rebase merge

--- config.js
+++ config.js
@@ -5,18 +5,18 @@ const googleMapAPI = 'AIzaSyB-j1k1E-e_Z7X4F-ZSQcPvTLR3V6_oLPY';
 const darkskyAPI = '053850fbd636dbb9267bd13fc145a22a';
 const geolocationAPI = 'AIzaSyBHX9MZGqPJG5YW4KGNQEdG-ZwkscWBQJE';
 
-const key = 'c03kxhLzv2Gk5NNDRZEEb8PCb7KZExJwbDFq4BjpGzEFzKO5bGRZiU0nnTYQUI2lPpWc6FPnNubOnmmg4josrU-yefmD-18RvUdhaKypUhjuhfWyD0is_DGUUXmLWXYx';
+const hotelkey = 'c03kxhLzv2Gk5NNDRZEEb8PCb7KZExJwbDFq4BjpGzEFzKO5bGRZiU0nnTYQUI2lPpWc6FPnNubOnmmg4josrU-yefmD-18RvUdhaKypUhjuhfWyD0is_DGUUXmLWXYx';
 
-const yelpkey = 'v2NdlEqwhnXc5yzlHQ6T6o5t0NmSperOsg7wmyMEpaIAV7_7CCx_jL41zskRGHKXJAgkrX8ocuvm6E39mbgdCH6F9W-ZijDrvkBfstXivjVVglGdvXi4_Nx9HwGKWXYx';
+const yelpKey = 'v2NdlEqwhnXc5yzlHQ6T6o5t0NmSperOsg7wmyMEpaIAV7_7CCx_jL41zskRGHKXJAgkrX8ocuvm6E39mbgdCH6F9W-ZijDrvkBfstXivjVVglGdvXi4_Nx9HwGKWXYx';
 
-// module.exports.flights = 'AIzaSyCDCZbj7Ath3p-jwi-ZmpAAEdWBmftH3r8';
-// module.exports.APCAuth = 'ea0eb61a9e';
-// module.exports.APCSecret = '4b35787cfc26306'
+module.exports.flights = 'AIzaSyCDCZbj7Ath3p-jwi-ZmpAAEdWBmftH3r8';
+module.exports.APCAuth = 'ea0eb61a9e';
+module.exports.APCSecret = '4b35787cfc26306'
 
 module.exports.clientId = clientId;
 module.exports.clientSecret = clientSecret;
-module.exports.yelpkey = yelpkey;
-module.exports.key = key;
+module.exports.yelpKey = yelpKey;
+module.exports.hotelkey = hotelkey;
 
 module.exports.googleMapAPI = googleMapAPI;
 module.exports.darkskyAPI = darkskyAPI;
