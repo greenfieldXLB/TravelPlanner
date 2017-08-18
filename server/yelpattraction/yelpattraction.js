c7fb4a0fdf2dca94462df3e00cfc8f2cb5282e38
Greg Susko <gsusko1@gmail.com>
2017-08-17 18:49:08 -0700

    update for heroku

--- server/yelpattraction/yelpattraction.js
+++ server/yelpattraction/yelpattraction.js
@@ -5,16 +5,16 @@ const yelpConfig = require('../../config.js');
 var searchAttr = function (searchCity, callback){
   var attrResult = {};
 
-  const clientId = yelpConfig.clientId;
-
-  const clientSecret = yelpConfig.clientSecret;
-
-  const token = yelp.accessToken(clientId, clientSecret).then(response => {
-
-    // console.log('TOKEN ', response.jsonBody.access_token);
-  }).catch(e => {
-    console.log('ERROR ', e);
-  });
+  // const clientId = yelpConfig.clientId;
+  //
+  // const clientSecret = yelpConfig.clientSecret;
+  //
+  // const token = yelp.accessToken(clientId, clientSecret).then(response => {
+  //
+  //   // console.log('TOKEN ', response.jsonBody.access_token);
+  // }).catch(e => {
+  //   console.log('ERROR ', e);
+  // });
 
 
   var yelpKey = process.env.YELP_KEY || yelpConfig.yelpKey;
