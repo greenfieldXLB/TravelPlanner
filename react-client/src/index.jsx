c00a289c9221c4e26b735b4bb5d206282a78694e
Greg Susko <gsusko1@gmail.com>
2017-08-18 13:12:28 -0700

    update for heroku

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -139,7 +139,6 @@ class App extends React.Component {
     var codes = {};
     var APCAuth = process.env.APC_AUTH || config.APCAuth;
     var APCSecret = process.env.APC_SECRET || config.APCSecret;
-    
     fetch(`https://www.air-port-codes.com/api/v1/multi?term=${departLoc}`, {
       headers: {
         Accept: "application/json",
