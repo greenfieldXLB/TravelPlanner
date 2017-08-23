4a2992ff35a0e93ccfab0963fcfcfb8bce20ef39
nealfwilliams <nealfwilliams@gmail.com>
2017-08-22 19:33:36 -0700

    Add Page changes

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -4,24 +4,9 @@ import $ from 'jquery';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Navbar from './components/Navbar.jsx';
 
-import LogIn from './components/logIn.jsx'
+import LogIn from './components/LogIn.jsx'
 
-/*
-import Hotels from './components/hotels.jsx'
-import Flights from './components/Flights.jsx';
-import config from '../../config.js';
-import SearchBar from './components/SearchBar.jsx';
-import Attraction from './components/Attraction.jsx';
-import FoodList from './components/FoodList.jsx';
-import Weather from './components/Weather.jsx';
-import SavedTrips from './components/savedTrips.jsx';
-*/
-
-const pages = {
-  LANDING: 'LANDING',
-  CREATE: 'CREATE',
-  LIST: 'LIST'
-}
+import * as pages from './pages';
 
 class App extends React.Component {
   constructor(props) {
@@ -31,7 +16,7 @@ class App extends React.Component {
       page: pages.LANDING
     }
     this.logIn = this.logIn.bind(this);
-    this.changePage = this.logIn.bind(this);
+    this.changePage = this.changePage.bind(this);
   }
 
   logIn(user) {
@@ -49,23 +34,26 @@ class App extends React.Component {
   render () {
     return (
       <MuiThemeProvider>
-        <Navbar />
-        { this.getMainComponent }
+        <div>
+          <Navbar
+            changePage={this.changePage}
+          />
+          { this.getMainComponent() }
+        </div>
       </MuiThemeProvider>
     )
   }
 
   getMainComponent() {
-    if (!this.state.user) {
+    /*if (!this.state.user) {
       return <LogIn login={this.login} />
-    }
-
+    }*/
     switch (this.state.page) {
       case pages.LANDING:
         return <div> Landing </div>
       case pages.CREATE:
         return <div> Create </div>
-      case pages.LOG_IN:
+      case pages.LIST:
         return <div> List </div>
     }
   }
