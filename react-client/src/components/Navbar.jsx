fa02bc09e0b323a46c5b961834f113effe2c295b
Miles Sorce <m.sorce@gmail.com>
2017-08-23 10:56:30 -0700

    (feat) Add Navbar

--- react-client/src/components/Navbar.jsx
+++ react-client/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import Avatar from 'material-ui/Avatar';
 import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
 
 import * as pages from '../pages';
@@ -8,33 +9,57 @@ class Navbar extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      value: 3,
-    };
-  }
-
-  handleChange (event, index, value) {
-    this.setState({value});
   }
 
   render() {
-    return (
-      <Toolbar>
-        <ToolbarGroup>
-          <RaisedButton
-            label="View Trips"
-            onClick={()=>{
-              this.props.changePage(pages.LIST);
-            }}
-          />
-          <ToolbarSeparator />
-          <RaisedButton
-            label="Create Trip"
-            onClick={()=>{
-              this.props.changePage(pages.CREATE);
-            }}
-          />
+    return ( 
+      <Toolbar
+        style={{
+          backgroundColor: 'white',
+          width: '100%',
+          paddingBottom: '35px'
+        }}
+      >
+        <ToolbarGroup firstChild={true}>
+          <div 
+            style={{paddingRight: '25px'}}
+          >
+            <img src="http://i.imgur.com/m2cZCRZ.png" 
+              style={{
+                height: '50px',
+                boxShadow: '0px 0px 0px'
+              }}
+            />
+          </div> 
         </ToolbarGroup>
+        <ToolbarGroup lastChild={true}>
+          <div 
+            style={{paddingRight: '15px'}}
+          >
+            <RaisedButton 
+              label="View Trips"
+              onClick={()=>{
+                this.props.changePage(pages.LIST);
+              }}
+              style={{marginRight: '3px'}}
+            />
+            <RaisedButton
+              label="Create Trip"
+              onClick={()=>{
+                this.props.changePage(pages.CREATE);
+              }}
+            />
+          </div>
+          <div>
+            <Avatar 
+              size={50} 
+              src="http://i.imgur.com/YxcBaYL.png"
+              style={{
+                boxShadow: '2px 2px 2px grey'
+              }} 
+            />
+          </div>
+        </ToolbarGroup> 
       </Toolbar>
     );
   }
