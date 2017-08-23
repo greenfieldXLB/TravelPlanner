d89c9b51c942f72c1e4db4932196c014bcef3cdb
nealfwilliams <nealfwilliams@gmail.com>
2017-08-23 14:23:28 -0700

    Add Landing Page

---
+++ react-client/src/components/Landing.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import FlatButton from 'material-ui/FlatButton';
+import * as pages from '../pages';
+
+const Landing = (props) => {
+  return (
+    <div style={{height: '100px'}}>
+      <FlatButton
+        label="Create Trip"
+        onClick={() => props.changePage(pages.CREATE)}
+      />
+
+      <FlatButton
+        label="View Trips"
+        onClick={() => props.changePage(pages.LIST)}
+      />
+    </div>
+  )
+};
+
+export default Landing;
