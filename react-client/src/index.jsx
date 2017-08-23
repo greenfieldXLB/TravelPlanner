6338a02d5e7bae45f569bbb0aa62a9e517e6c04e
nealfwilliams <nealfwilliams@gmail.com>
2017-08-23 12:18:18 -0700

    Basic login added

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider.js';
+import _ from 'lodash';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import CreateView from './components/CreateView/index.jsx';
 import Navbar from './components/Navbar.jsx';
 
 import LogIn from './components/LogIn.jsx'
 
+import * as styles from './styles';
 import * as pages from './pages';
 
 class App extends React.Component {
@@ -17,55 +19,14 @@ class App extends React.Component {
       user: null,
       page: pages.LANDING,
     };
-    this.onSearch = this.onSearch.bind(this);
-  }
-
-  componentDidMount() {
-  }
-
-  hotelsSearch() {
-     $.ajax({
-      url: '/hotels',
-      method: 'GET',
-      data: {city: this.state.arrivalLocation},
-      success: (res) => {
-        const parsedHotel = JSON.parse( res );
-        const addHotelAddress = this.state.addresses
-        .concat( parsedHotel.map( this.responseToSaveAddress( 'hotel' ) ) );
-
-        this.setState({
-          hotels: parsedHotel,
-          addresses: addHotelAddress
-        });
-      },
-      error: (err) => {
-        console.log('error !')
-      }
-    })
-  }
-
-  handleHotelClick(hotel, event){
-    console.log(hotel.url);
-
-    this.removeClass('hotelHighlight');
-    if (this.state.selectedHotelId === hotel.id) {
-      this.state.savedChoices[0].hotel = {};
-      delete this.state.selectedHotelId;
-    } else {
-      this.setState({
-        selectedHotelId: hotel.id
-      });
-      $(event.target).toggleClass('hotelHighlight');
-      var saved = {
-        name: hotel.name,
-        address: hotel.location.display_address.join(', '),
-        price: hotel.price,
-        image_url: hotel.image_url
-      };
-     this.state.savedChoices[0].hotel = saved;
-    }
     this.logIn = this.logIn.bind(this);
     this.changePage = this.changePage.bind(this);
+  }
+
+  changePage(page) {
+    this.setState({
+      page
+    });
   }
 
   logIn(user) {
@@ -74,47 +35,46 @@ class App extends React.Component {
     });
   }
 
-  removeClass(classname) {
-    var elems = document.querySelectorAll(`.${classname}`);
-    elems.forEach(ele => {
-      ele.classList.remove(classname);
-    });
-  }
-
-  onSearch (departureLocation, arrivalLocation, departureDate, returnDate) {
-    console.log('the departure location is: ', departureLocation);
-    console.log('the arrival location is: ', arrivalLocation);
-    console.log('the departure date is: ', departureDate);
-    console.log('the return date is: ', returnDate);
-    this.removeClass('flightHighlight');
-    this.removeClass('hotelHighlight');
-    this.setState({
-      page
-    });
-  }
-
   render () {
     return (
       <MuiThemeProvider>
-        <div style={{
-          height: '100%'
-        }}>
-          <Navbar
-            changePage={this.changePage}
-          />
-          { this.getMainComponent() }
+        <div>
+          {
+            this.state.user && this.state.page !== pages.LANDING ?
+
+            <div>
+              <Navbar
+                changePage={this.changePage}
+              />
+              { this.getMainComponent() }
+            </div>
+
+            :
+
+            <div style={_.extend(
+              styles.CENTERED,
+              styles.FIXED_PAGE,
+              {flexFlow: 'column'}
+            )} >
+              <img
+                style={{width: '500px', marginBottom: '20px'}}
+                src='http://i.imgur.com/m2cZCRZ.png'
+              />
+              { this.getMainComponent() }
+            </div>
+          }
         </div>
       </MuiThemeProvider>
     )
   }
 
   getMainComponent() {
-    /*if (!this.state.user) {
-      return <LogIn login={this.login} />
-    }*/
+    if (!this.state.user) {
+      return <LogIn logIn={this.logIn} />
+    }
     switch (this.state.page) {
       case pages.LANDING:
-        return <div> Landing </div>
+        return <div style={{height: '100px'}}> Landing </div>
       case pages.CREATE:
         return <div> Create </div>
       case pages.LIST:
