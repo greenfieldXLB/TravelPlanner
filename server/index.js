ed751f140a9fcc2056c2b81f03f76b4f080de294
Thomas Cosby <cosbyts@gmail.com>
2017-08-23 17:54:16 -0700

    Removed commented-out code.

--- server/index.js
+++ server/index.js
@@ -10,46 +10,25 @@ const food = require('./yelpfood/yelpfood')
 app.use(express.static(__dirname + '/../react-client/dist'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
 app.get('/hotels', (req, res) => {
-  // console.log('reached server for /hotels : ', req.query);
   hotels.findHotels(req.query, (hotels) => {
     res.status(200).send({tag: 'hotels', data: hotels});
   });
 });
 
 app.get('/attractions', (req, res) => {
-  // console.log('reached server /attractions : ', req.query);
   attractions.findAttractions(req.query, (attractions) => {
     res.status(200).send({tag: 'attractions', data: attractions});
   });
 });
 
 app.get('/food', (req, res) => {
-  // console.log('reached server /food : ', req.query);
   food.findRestaurants(req.query, (restaurants) => {
     res.status(200).send({tag: 'restaurants', data: restaurants});
   });
 });
 
-// app.post('/attraction', function(req,res){
-//   const attrLocation = req.body.location;
-//   yelpattr.searchAttr(attrLocation, function(attrResult){
-//     res.send(200, JSON.stringify(attrResult));
-//   })
-// })
-
-// app.get('/hotels', (req, res) => {
-//   hotel.hotel(req.query, (data) => {
-//     res.end(JSON.stringify(data))
-//   })
-// })
-
-// app.post('/food', function (req, res){
-//   let location = req.body.location;
-//   yelpfood.searchFood(location, function(foodresult){
-//     res.send(200, JSON.stringify(foodresult));
-//   });
-// });
 
 app.post('/save', (req, res) => {
   var data = JSON.parse(req.body.data);
